b8dba795af36b72a040ebd26f9fbed608cc5ff26
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-07-01 23:23:54 +0800

    working closing mechanism; run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import subprocess
+import psutil
+import time
+
+def is_process_running(pid):    
+    return psutil.pid_exists(pid)
+
+if __name__ == '__main__':
+    watchdog_proc = subprocess.Popen([".venv/Scripts/python", "./test.py"])
+    notepad_proc = subprocess.Popen(["notepad", "./input.txt"])
+
+    try:
+        while is_process_running(notepad_proc.pid):
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        watchdog_proc.terminate()
+        watchdog_proc.wait()
