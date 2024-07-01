044bec0c44fe0aa906c294dc4faac2eca6716f87
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-07-01 21:24:50 +0800

    allow using input txt file, put txt to image

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
-desktop_size = (1920, 1080)
 
-# Create a new image with RGB mode
-image = Image.new("RGB", desktop_size, "white")
+def parse_config(txt_file):
+    with open(txt_file, "r") as file:
+        config = {}
+        for line in file:
+            if line.startswith("#"):
+                continue
 
-# Draw on the image
-draw = ImageDraw.Draw(image)
-draw.rectangle([100, 100, 300, 300], outline="black", width=5)
+            key, value = line.split("=")
+            config[key.strip()] = value.strip()
 
-# Save image
-image.save("./output/output.png")
+    return config
+
+def parse_input(txt_file):
+    with open(txt_file, "r") as file:
+        text = file.read()
+    
+    return text
+
+if __name__ == "__main__":
+
+    TEXT = parse_input(txt_file = "input.txt")
+    CONFIG = parse_config(txt_file = "config.txt")
+
+    # Create image
+    desktop_height = int(CONFIG['desktop_height'])
+    desktop_width = int(CONFIG['desktop_width'])
+    desktop_size = (desktop_width, desktop_height)
+    image = Image.new("RGB", (int(CONFIG['desktop_width']), int(CONFIG['desktop_height'])), CONFIG['background_color'])
+
+    # Draw on the image
+    draw = ImageDraw.Draw(image)
+
+    # Draw Text on Image
+    text_position = (desktop_width // 2, desktop_height // 2)
+    text_color = CONFIG['text_color']
+    text_size = int(CONFIG['text_size'])
+
+    # Dynamically change text size based on number of lines in the text
+    # lines = TEXT.split("\n")
+    # num_lines = len(lines)
+    # text_size = text_size - (num_lines - 1) * 8
+
+    # Create a font object with the desired size
+    font = ImageFont.truetype("arial.ttf", text_size)
+
+    draw.multiline_text(text_position, TEXT, fill=text_color, font=font, anchor='mm')
+
+    # Save image
+    image.save("./output/output.png")
+
+    # TODO
+    # - Read Text from TXT File
+    # - Draw Text on Image
+    # - Dynamically change text size based on number of lines in the text
